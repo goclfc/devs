cff4d7bc0b1bd56d01773716b30efba121cde8d5
GatsbyJr <gocha.berulava@gmail.com>
2023-03-15 14:11:57 +0400

    create app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App text-3xl bg-purple">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
